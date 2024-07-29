887eb9a6b2addf190df3537abc8ef94c971a5607
RomaaRio <baziukroman23@gmail.com>
2024-07-29 21:24:08 +0300

    Add fun CreateCategory

---
+++ WebASP.NET_study/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebASP.NET_study.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
