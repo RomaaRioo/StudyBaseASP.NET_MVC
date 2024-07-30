a4e716cf27e008e7d7643cbddbf7a319329f2e7e
RomaaRio <baziukroman23@gmail.com>
2024-07-30 21:15:01 +0300

    Add edit category

--- WebASP.NET_study/Controllers/CategoryController.cs
+++ WebASP.NET_study/Controllers/CategoryController.cs
@@ -30,6 +30,10 @@ namespace WebASP.NET_study.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrdere.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -38,5 +42,42 @@ namespace WebASP.NET_study.Controllers
             }
             return View(obj);
         }
-    } 
+
+		//GET
+		public IActionResult Edit(int? id)
+		{
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoreFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+			//var categoreFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+			
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            
+            return View(categoryFromDb);
+		}
+
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrdere.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(obj);
+		}
+	} 
 }
